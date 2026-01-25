a720506ad67261d744dcb997af77e3cc367d717e
Alaa Albast <eduway.dev@yahoo.com>
2026-01-25 11:39:39 +0200

    fix testing issues

--- test/test_intergration.py
+++ test/test_intergration.py
@@ -2,8 +2,8 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from main import app
-from database import Base, get_db
+from app.main import app
+from app.database import Base, get_db
 
 # --- TEST SETUP ---
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test_integration.db"
