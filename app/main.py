dd51f0ca7fb8035f89c80cf4cfe68ae8bb72d211
Eduway <eduway.dev@yahoo.com>
2026-02-04 19:35:18 +0200

    fix student dashboard

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from database import engine, Base
-from routes import doctors, students, ratings ,studentInfo,doctorInfo,ml_predictions
+#from database import engine, Base
+#from routes import doctors, students, ratings ,studentInfo,doctorInfo,ml_predictions
 from fastapi.middleware.cors import CORSMiddleware
+
+# To this:
+from app.database import engine, Base
+from app.routes import doctors, students, ratings, studentInfo, doctorInfo, ml_predictions
 
 # This command triggers the creation of tables in PostgreSQL
 # It checks if they exist; if not, it creates them.
